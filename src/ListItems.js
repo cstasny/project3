2d3cc4253645a4cca4d285b640037071ce2d3bf3
Clifford Stasny <stasser1@outlook.com>
2020-06-03 01:52:17 -0500

    added delete option

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import './ListItems.css'
+import './ListItems.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import FlipMove from 'react-flip-move';
 
 function ListItems(props){
     const items = props.items;
     const listItems = items.map(item =>
-        {
-            return <div className="list" key="item.key">
-                <p>{item.text}
-                <span>
-                    <FontAwesomeIcon className="faicons" icon="trash"/>
-                </span>
-                </p>
+   {
+       return <div className="list" key={item.key}>
+     <p>
+         <input type="text" id={item.key} value={item.text} onChange={(e)=>{
+             props.setUpdate(e.target.value,item.key)}}/>
+        <span>
+       
+        <FontAwesomeIcon className="faicons" onClick={() => {
+            props.deleteItem(item.key)
+        }} icon="trash" />
+        </span>
+     </p>
+     
+    </div>})
+    return <div>
+        <FlipMove duration={300} easing="ease-in-out">
+        {listItems}
+        </FlipMove>
+    
+    </div>;
+  }
 
-            </div>
-        })
-    return(
-        <div>{listItems}</div>
-    )
-}
-export default ListItems;
+  export default ListItems;
