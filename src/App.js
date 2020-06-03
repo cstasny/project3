212cd88c08a15b8c59ea66092be1e7372815b42e
Clifford Stasny <stasser1@outlook.com>
2020-06-03 01:35:11 -0500

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ListItems from './ListItems'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTrash);
 
 class App extends React.Component{
   constructor(props){
@@ -26,6 +31,16 @@ class App extends React.Component{
   addItem(e){
    e.preventDefault();
    const newItem = this.state.currentItem;
+   if(newItem.text!==""){
+     const newItems=[...this.state.items, newItem];
+     this.setState({
+       items:newItems,
+       currentItem:{
+         text:'',
+         key:''
+       }
+     })
+   }
   }
   render(){
     return (
@@ -38,6 +53,7 @@ class App extends React.Component{
            <button type="submit">Add</button>
           </form>
         </header>
+        <ListItems items = {this.state.items}></ListItems>
         </div>
       );
     }
