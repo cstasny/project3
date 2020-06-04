79c2b3219ca7728b2bacfea9fc20d9a2f37de3d7
Clifford Stasny <stasser1@outlook.com>
2020-06-04 03:28:11 -0500

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ListItems from './ListItems'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import ListItems from './ListItems';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+
 
 library.add(faTrash)
 
@@ -69,8 +70,12 @@ class App extends React.Component {
   }
  render(){
   return (
-    <div className="App">
-      <header>
+    <div>
+    <nav className="navbar">
+      
+    </nav>
+    <div>
+      <header className="App">
         <form id="to-do-form" onSubmit={this.addItem}>
           <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
           <button type="submit">Add</button>
@@ -80,6 +85,7 @@ class App extends React.Component {
           <ListItems items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate}/>
         
       </header>
+    </div>
     </div>
   );
  }
